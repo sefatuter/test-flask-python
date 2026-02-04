3512add2a2e9877971523b839ea1cb83a0c29dfa
sefa <95074982+sefatuter@users.noreply.github.com>
2026-02-04 18:34:25 +0300

    Update home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask! Test Webhook ngrok docker github actions test? working now."
+    return "Hello, Flask! Test Webhook ngrok docker github actions test? working now. and perfectly"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
