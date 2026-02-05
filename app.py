91b5b4518b0a67129dce515228df58df012800e2
sefa <95074982+sefatuter@users.noreply.github.com>
2026-02-05 18:07:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask! Test " 
+    return "Hello, Flask! Test a" 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
