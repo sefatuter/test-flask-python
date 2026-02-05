17b62c4451acbe3e36bd16489a9892e730eba318
sefa <95074982+sefatuter@users.noreply.github.com>
2026-02-05 18:48:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask! Test abc" 
+    return "Hello, Flask! Test abc cache working?" 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
